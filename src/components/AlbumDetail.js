cd4073f771ff6dde34f23b4a115be416b161463f
Julian Scharf <jscharf@thoughtworks.com>
2017-05-08 20:18:34 +1000

    [JS] Lecture 50 complete

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
   const {
@@ -38,6 +39,10 @@ const AlbumDetail = ({ album }) => {
       style={imageStyle}
       source={{ uri: image }}
       />
+      </CardSection>
+
+      <CardSection>
+      <Button onPress={() => console.log(title)} />
       </CardSection>
     </Card>
   );
